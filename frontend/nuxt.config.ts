33c1078a3e1228feaf3d257f936e9a8f13e9038e
KhanhNguyenVimaru <khanhnd05@gmail.com>
2026-04-05 13:40:30 +0700

    first commit

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,26 @@
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  }
+})
